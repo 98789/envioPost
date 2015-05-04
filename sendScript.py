b4fd804860bf2acc6f0625930f60e1203abac7c8
root <root@localhost.localdomain>
2015-05-04 11:56:02 -0500

    Enviar datos al ws mediante python

---
+++ sendScript.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from json import dumps
+import requests
+
+def sendJson(payload, url = 'http://192.168.45.156/sensores/web/mediciones', type = 'json'):
+
+  if type == 'json':
+    headers = {'content-type': 'application/json'}
+  elif type == 'xml':
+    headers = {'content-type': 'application/xml'}
+  else:
+    print "unknown content type"
+    return -1
+
+  r = requests.post(url, data=dumps(payload), headers=headers)
+
+  responses = {
+  1: '',
+  2: 'Los datos han sido almacenados con éxito.',
+  3: '',
+  4: 'Error, no se encontraron datos o el formato es incorrecto.',
+  5: 'Error, los datos contienen valores no válidos.'
+  }
+
+  print r.status_code,': ',responses[r.status_code/100]
+
+  return 1
+
+
+def sendRaw(date, band, value, sensor_id, type_id, comment = None,
+url = 'http://192.168.45.156/sensores/web/mediciones', type = 'json'):
+
+  payload = {"fecha_toma": date, "banda": band, "valor_medido": value, "sensor": sensor_id, "tipo": type_id, "comentario": comment}
+  sendJson(payload)
+  return 1
