2bda18a89c943941b51039ff152d4f65b468d45c
98789 <lerht@hotmail.com>
2015-06-03 13:49:28 -0500

    Corregidas coordenadas del script

--- genRandMeasure.py
+++ genRandMeasure.py
@@ -12,7 +12,7 @@ for i in range(5):
 sensor_id = 2
 type_id = randint(3)
 longitude = -72 + randint(-20000,20000)/10000.0
-latitude = -7 + randint(-5000,5000)/10000.0
+latitude = 7 + randint(-5000,5000)/10000.0
 comment = "Automatically generated from python script"
 
 sendRaw(date, tag, value, sensor_id, type_id, longitude, latitude, comment)
