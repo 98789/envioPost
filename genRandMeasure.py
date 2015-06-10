439d8aafbc8a2ba2fc8dd640e92b2d8bf00f5ad4
98789 <lerht@hotmail.com>
2015-06-10 11:08:43 -0500

    Update to match new function names

--- genRandMeasure.py
+++ genRandMeasure.py
@@ -1,19 +1,17 @@
-from WSConsumer import  sendRaw
+from consumer import  send_raw
 import datetime
 from numpy.random import randint
 
 date = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 tag = randint(1,11)
-value = ""
-for i in range(randint(1,17)):
-    for j in range (randint(1,201)):
-        value+=str(randint(20000))+" "
-    value = value[0:-1]+";"
+limit_i = randint(1,17)
+limit_j = randint(1,201)
+value = ';'.join(' '.join(str(randint(20000)) for j in range(limit_j)) for i in range(limit_i))
 value = value[0:-1]
 sensor_id = 2
 type_id = randint(1,4)
-longitude = -72 + randint(-20000,20000)/10000.0
-latitude = 7 + randint(-10000,10000)/10000.0
+long = -72 + randint(-20000,20000)/10000.0
+lat = 7 + randint(-10000,10000)/10000.0
 comment = "Automatically generated from python script"
 
-sendRaw(date, tag, value, sensor_id, type_id, longitude, latitude, comment)
+send_raw(date, value, sensor_id, type_id, long, lat, comment, tag = tag)
